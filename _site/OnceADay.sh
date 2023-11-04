dd5311f79a21dab79addd06b7b67d25b941aca7d
Liujiyu20000308 <3170104256@zju.edu.cn>
2023-11-04 21:47:14 +0800

    The day is over.

--- _site/OnceADay.sh
+++ _site/OnceADay.sh
@@ -3,4 +3,4 @@ rm -f ./_posts/*.html
 echo "have removed all the html file in the './_posts' directory"
 git add .
 git commit -m "The day is over."
-git push -u origin main
+git push -u origin master
